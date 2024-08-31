d4bcf2b92307d5f34cfc39fcac4610d0005543eb
Gouthami406 <gowthamihm406@gmail.com>
2024-08-31 13:13:00 +0530

    Create pointers.c

---
+++ pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+    int *a;
+    float *b;
+    char *c;
+    printf("size of interger pointer is %d\n", sizeof(a));
+    printf("size of float pointer is %d\n", sizeof(b));
+    printf("size of charecter pointer is %d\n", sizeof(c));
+    int d =10;
+    a=&d;
+    printf("address of d is %d\n",a);
+    printf("value of d is %d\n",*a);
+}
