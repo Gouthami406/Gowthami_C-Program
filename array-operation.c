9b6baeedc48681744f3fcdf10ddde89d29811a00
Gouthami406 <gowthamihm406@gmail.com>
2024-08-31 13:20:21 +0530

    Create array-operation.c

---
+++ array-operation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 50
+int arr[MAXSIZE];
+int size=0;
+int display()
+{
+  int i;
+  if(size==0)
+  {
+    printf("Array is empty");
+  }
+  for(i=0;i<size;i++)
+    {
+arr
